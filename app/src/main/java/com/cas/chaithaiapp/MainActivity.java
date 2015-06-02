fe06f4c0f11b7b1abd21f29df4b1b3b25e9a72d6
Chris Buruchian <Chris_12456@hotmail.com>
2015-06-01 22:39:59 -0400

    Experimented with the title screen.

--- app/src/main/java/com/cas/chaithaiapp/MainActivity.java
+++ app/src/main/java/com/cas/chaithaiapp/MainActivity.java
@@ -1,23 +1,30 @@
 package com.cas.chaithaiapp;
 
+import android.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setDisplayShowHomeEnabled(true);
+        ab.setIcon(R.mipmap.ic_launcher);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
         return true;
     }
 
