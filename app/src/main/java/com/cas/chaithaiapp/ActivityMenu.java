fdca86ca296d79e60e86a9e34eff4bcb83782787
Chris Buruchian <Chris_12456@hotmail.com>
2015-06-04 02:37:15 -0400

    Added a new activity and sample buttons to represent design idea.

--- app/src/main/java/com/cas/chaithaiapp/ActivityMenu.java
+++ app/src/main/java/com/cas/chaithaiapp/ActivityMenu.java
@@ -1,39 +1,23 @@
 package com.cas.chaithaiapp;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-//import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity{
+
+public class ActivityMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button menuButton = (Button) findViewById(R.id.ldmenuButton);
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,ActivityMenu.class));
-            }
-        });
-//        android.support.v7.app.ActionBar ab = getSupportActionBar();
-//        ab.setDisplayShowHomeEnabled(true);
-//        ab.setIcon(R.mipmap.ic_launcher);
+        setContentView(R.layout.activity_test);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-
+        getMenuInflater().inflate(R.menu.menu_test, menu);
         return true;
     }
 
